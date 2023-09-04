303641c5a4484fa2ae84e2ef1f360edaa8dde82e
luciagarciabes <lgarciabes@udesa.edu.ar>
2023-09-04 08:32:43 -0300

    peliculascontainter

---
+++ proyecto3/src/components/PeliculasContainer/PeliculasContainer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Pelicula from '../Pelicula/Pelicula'
+
+export default function PeliculasContainer() {
+  return (
+    <section className="seccion_peliculas">
+        <br/><br/>
+
+        <ul className="lista_principal">
+            
+       
+        </ul>
+
+     </section>
+  )
+}
